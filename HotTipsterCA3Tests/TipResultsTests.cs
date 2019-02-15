e0351ffe1d84d33a079f9225af80a4f41cdab434
barrykeegan <barry.keegan@gmail.com>
2019-02-15 13:06:17 +0000

    changed tests to have a global tipresults variable so it is not being instantiated for each test method

--- HotTipsterCA3Tests/TipResultsTests.cs
+++ HotTipsterCA3Tests/TipResultsTests.cs
@@ -11,20 +11,23 @@ namespace HotTipsterCA3.Tests
     [TestClass()]
     public class TipResultsTests
     {
-        
-        
+        private static TipResults Results = new TipResults();
+
+        private void AddFirstResult(TipResults results)
+        {
+            results.Add(new TipResult { Course = "Aintree", RaceDate = new DateTime(2017, 05, 12), ResultValue = 11.58m, Won = true });
+        }
+
         [TestMethod()]
         public void AddTest()
         {
-            TipResults Results = new TipResults();
-            Results.Add(new TipResult { Course = "Aintree", RaceDate = new DateTime(2017, 05, 12), ResultValue = 11.58m, Won = true });
+            AddFirstResult(Results);
             Assert.AreEqual(1, Results.Count);
         }
 
         [TestMethod()]
         public void CountTest()
         {
-            TipResults Results = new TipResults();
             Assert.AreEqual(0, Results.Count);
         }
     }

3864b48185d44cd7b68074287503c1014fd6a55a
barrykeegan <barry.keegan@gmail.com>
2019-02-15 11:48:07 +0000

    Added Count test to test TipResults.Count, count should be 0 on newly initialised list

--- HotTipsterCA3Tests/TipResultsTests.cs
+++ HotTipsterCA3Tests/TipResultsTests.cs
@@ -11,10 +11,19 @@ namespace HotTipsterCA3.Tests
     [TestClass()]
     public class TipResultsTests
     {
+        
+        
         [TestMethod()]
         public void AddTest()
         {
             Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CountTest()
+        {
+            TipResults Results = new TipResults();
+            Assert.AreEqual(0, Results.Count);
         }
     }
 }

dfc9097d1178628210fa07a7ce4e52728b4b8ab2
barrykeegan <barry.keegan@gmail.com>
2019-02-16 13:41:52 +0000

    Implemented IsValidResult to validate a TipResult object, made use of App.config for one of the values

--- HotTipsterCA3/TipResult.cs
+++ HotTipsterCA3/TipResult.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +24,27 @@ namespace HotTipsterCA3
 
         public bool IsValidResult()
         {
-            
+            if(string.IsNullOrEmpty(Course))
+            {
+                return false;
+            }
+            //Earliest tip results available are from 2016, this value is added to App.Config AppSettings key 
+            //"earliestTipYear". This allows for config file to be edited simply, to allow for Tips from 
+            //earlier years.
+            //Also tipResults from the future can't be known, so if RaceDate is greater than now it can't be valid data
+            if (RaceDate.Year < int.Parse(ConfigurationManager.AppSettings["earliestTipYear"]) || RaceDate > DateTime.Now)
+            {
+                return false;
+            }
+            //From sample data HotTipster seems to bet a minimum of 10 euro on a race. 
+            //If value less than that, then not a valid bet value.
+            //If HotTipster won his bet, the value will also be bigger than the amount bet.
+            if (ResultValue < 10.00m)
+            {
+                return false;
+            }
+            //There is no real way to validate Won property, 
+            //since it will initialise to false which is a valid value
             return true;
         }
     }
